d0361e838070d1eef7935ed9b54af9d47d9afdbf
Wenega Wama <narcissewatherloy@gmail.com>
2021-05-15 16:17:59 -0300

    Added menu toggle imperfeito

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-//Validação do formulario
-//document.addEventListener('DOMContentLoaded')
+/*Menu mobile*/
+const btnMobile = document.querySelector('#btn-mobile')
 
-// document.querySelector('form').addEventListener('submit', event => {
-//     console.log('enviar o formulario')
+function toggleMenu(){
+    const nav = document.querySelector('#nav')
 
-//     //não vai enviar o formulario
-//     //event.preventDefault()
-// })
+    nav.classList.toggle('active')
+}
 
-// const campos = document.querySelectorAll('[required]')
-// //console.log(campos)
-// function custumValidition(event){
-//     const campo = event.target
-//     console.log(campo.validity)
+btnMobile.addEventListener('click',toggleMenu)
 
-//     //trocar messagem
-//     campo.setCustomValidity('Esse campo é obligatorio')
-// }
 
-// for(campo of campos){
-//     campo.addEventListener('invalido',custumValidation)
-// }
 
-//scroll suave
-//Identificar o clique no menu
-//verificar o item que foi clicado e fazer referencia com o alvo
-//verificar a distancia entre o alvo 
-//animar o scroll até o alvo
+
+
+/*scroll suave
+Identificar o clique no menu
+verificar o item que foi clicado e fazer referencia com o alvo
+verificar a distancia entre o alvo 
+animar o scroll até o alvo*/
 
 
 const menuItems = document.querySelectorAll(".menu-nav a[href^='#']")
@@ -56,25 +47,55 @@ function getScrollTopByHref(element){
 
 
 
-// const contacto = document.querySelector('#contact')
-// const texto = document.querySelector('.texto')
 
-// function handleKeyUp(event){
-//     const target = event.target
 
-//     if(!target.checkValidity()){
-//         target.classList.add('invalido')
-//         contacto.nome.setCustomValidity('Preecher seu nome por favor')
-//         contacto.email.setCustomValidity('Preecher um email válido por favor')
-//         contacto.assunto.setCustomValidity('Est campo é obligatório ')
-//         contacto.mensagem.setCustomValidity('Digitar sua messagem')
-//         target.nextElementSibling.innerText = target.validationMessage
-//     } else {
-//         target.classList.remove('invalido')
-//     }
-//     //console.log(event.target.checkValidity())
-//     //console.log(event.target.value)
-//     //texto.innerHTML = event.target.value
-// }
+//Validação do formulario
+//document.addEventListener('DOMContentLoaded')
 
-// contacto.addEventListener('change',handleKeyUp)
+ document.querySelector('form').addEventListener('submit', event => {
+     console.log('enviar o formulario')
+
+     //não vai enviar o formulario
+     event.preventDefault()
+ })
+
+const campos = document.querySelectorAll('[required]')
+console.log(campos)
+function custumValidation(event){
+    const campo = event.target
+    console.log(campo.validity)
+
+    //trocar messagem
+    campo.setCustomValidity('Esse campo é obligatorio')
+}
+
+for(campo of campos){
+    campo.addEventListener('invalido',custumValidation)
+}
+
+
+
+
+
+const contacto = document.querySelector('#contact')
+const texto = document.querySelector('.texto')
+
+function handleKeyUp(event){
+    const target = event.target
+
+    if(!target.checkValidity()){
+        target.classList.add('invalido')
+        contacto.nome.setCustomValidity('Preecher seu nome por favor')
+        contacto.email.setCustomValidity('Preecher um email válido por favor')
+        contacto.assunto.setCustomValidity('Est campo é obligatório ')
+        contacto.mensagem.setCustomValidity('Digitar sua messagem')
+        target.nextElementSibling.innerText = target.validationMessage
+    } else {
+        target.classList.remove('invalido')
+    }
+    //console.log(event.target.checkValidity())
+    //console.log(event.target.value)
+    //texto.innerHTML = event.target.value
+}
+
+contacto.addEventListener('change',handleKeyUp)
